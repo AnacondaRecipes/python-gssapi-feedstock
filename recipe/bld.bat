9b4312840d29cc4fb97eaa10a328f3385490a369
Serhii Kupriienko <skupriienko@anaconda.com>
2024-04-24 11:29:36 +0300

    Debugging

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,7 @@ setlocal EnableDelayedExpansion
 echo on
 
 set GSSAPI_LINKER_ARGS=-lgssapi64
-set GSSAPI_COMPILER_ARGS="-fPIC -DMS_WIN64"
+set GSSAPI_COMPILER_ARGS=-DMS_WIN64
 set GSSAPI_MAIN_LIB=%LIBRARY_BIN%\gssapi64.dll
 
 %PYTHON% -m pip install . -vv
