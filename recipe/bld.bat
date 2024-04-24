70ce0f5dd048d1834b50fdee8ae7c0d82e9ccd53
Serhii Kupriienko <skupriienko@anaconda.com>
2024-04-24 11:56:33 +0300

    Debugging

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,4 +6,4 @@ set GSSAPI_LINKER_ARGS=-lgssapi64
 set GSSAPI_COMPILER_ARGS=-DMS_WIN64
 set GSSAPI_MAIN_LIB=%LIBRARY_BIN%\gssapi64.dll
 
-%PYTHON% -m pip install . -vv
+%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
