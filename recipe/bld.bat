447e86c8bbf03c435f4e04d5273d60a4f410267e
Mikolaj Gralczyk <mgralczyk@anaconda.com>
2026-01-23 12:47:48 +0100

    Update to 1.10.1

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,8 @@
 setlocal EnableDelayedExpansion
 
 echo on
-
-set GSSAPI_LINKER_ARGS=-lgssapi64
-set GSSAPI_COMPILER_ARGS=-DMS_WIN64
 set GSSAPI_MAIN_LIB=%LIBRARY_BIN%\gssapi64.dll
+set "GSSAPI_LINKER_ARGS=gssapi64.lib krb5_64.lib"
+set GSSAPI_COMPILER_ARGS=-DMS_WIN64
 
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
